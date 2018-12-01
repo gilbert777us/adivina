11435d54d8820c85bd553a7e5baf0790a126fe94
gilbert777us <poliestireno@gmail.com>
2018-12-01 21:06:29 +0100

    Update UJ7L6K2J4HF32Y495HG5B5V3A.php
    para atras

--- UJ7L6K2J4HF32Y495HG5B5V3A.php
+++ UJ7L6K2J4HF32Y495HG5B5V3A.php
@@ -1,10 +1,6 @@
 <!DOCTYPE HTML>  
 <?php 
-  	session_start();
-  	if ($_SESSION["entrar"]==2)
-    {
-        header("Location:UJ7L6K0J4HF32Y495HG5B5V3A.php");
-    }
+session_start(); 
 function getNombreCarta($c)
 {
   	// decodificación del get de la carta
